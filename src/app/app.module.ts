691167e32aaf8901f95af9dea62149613f81aa73
Ismael Dominguez <ismaeldb_@hotmail.com>
2022-04-29 17:39:31 -0500

    feat: crear modulo vehicle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehicleModule } from './vehicle/vehicle.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VehicleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
