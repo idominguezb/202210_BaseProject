2b840ca49d7f8ccc2dbe103540a8e4f7136eba85
Ismael Dominguez <ismaeldb_@hotmail.com>
2022-04-29 19:14:24 -0500

    punto 2: agregar marcas y cantidades

--- src/app/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -7,17 +7,34 @@ import { VehicleService } from '../vehicle.service';
   templateUrl: './vehicle-list.component.html',
   styleUrls: ['./vehicle-list.component.css']
 })
+
 export class VehicleListComponent implements OnInit {
 
   vehicles: Array<Vehicle> = [];
+  brandsNumbers = {}
 
   constructor(private bookService: VehicleService) { }
 
   getVehicles(): void {
     this.bookService.getVehicles().subscribe((vehicles) => {
       this.vehicles = vehicles;
+      this.getBrandsNumbers();
     });
   }
+
+  getBrandsNumbers() {
+    const brands = this.vehicles.map(vehicle => vehicle.marca);
+    const brandsNumbers : { [key: string]: number }  = {}
+
+    for (let i = 0; i < brands.length; i++) {
+      const brand = brands[i];
+      brandsNumbers[brand] = (brandsNumbers[brand] || 0) + 1
+    }
+
+    this.brandsNumbers = brandsNumbers
+  }
+
+
   ngOnInit() {
     this.getVehicles()
   }
