c395bbbff4818dd0d3bfa234b5c29781124d89ca
Ismael Dominguez <ismaeldb_@hotmail.com>
2022-04-29 20:02:20 -0500

    punto 3: probar componente

--- src/app/vehicle/vehicle-list/vehicle-list.component.spec.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.spec.ts
@@ -1,16 +1,19 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { VehicleListComponent } from './vehicle-list.component';
+import { Vehicle } from '../vehicle';
+import { faker } from '@faker-js/faker';
 
 describe('VehicleListComponent', () => {
   let component: VehicleListComponent;
   let fixture: ComponentFixture<VehicleListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ VehicleListComponent ]
     })
     .compileComponents();
@@ -19,10 +22,34 @@ describe('VehicleListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehicleListComponent);
     component = fixture.componentInstance;
+
+    const vehicles = [];
+
+    for (let i = 0; i < 3; i++) {
+      vehicles.push(
+        new Vehicle(
+          faker.datatype.number(),
+          faker.random.word(),
+          faker.random.word(),
+          faker.random.word(),
+          faker.vehicle.model(),
+          faker.random.word(),
+          faker.vehicle.color(),
+          faker.image.avatar()
+        )
+      )
+    }
+    component.vehicles = vehicles;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render head', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('table thead').length).toEqual(1);
+  });
+
+  it('should have 3 elements', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('table tbody tr').length).toEqual(3);
   });
 });
--- src/app/vehicle/vehicle.service.spec.ts
+++ src/app/vehicle/vehicle.service.spec.ts
@@ -1,11 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { VehicleService } from './vehicle.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Vehicle', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [VehicleService]
     });
   });
